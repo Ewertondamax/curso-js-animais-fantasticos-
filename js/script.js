ff6df0682a38bedc07a47313d7df0c317f6c3a4f
Ewerton Martins da Silva <ewertondamax@gmail.com>
2022-12-03 11:03:08 -0300

    implementado js no site

--- js/script.js
+++ js/script.js
@@ -1,8 +1,61 @@
-// Retorne o url da página atual utilizando o objeto window
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
 
-// Seleciona o primeiro elemento da página que
-// possua a classe ativo
+  if (tabMenu.length && tabConteudo.length) {
+    tabConteudo[0].classList.add("ativo");
 
-// Retorne a linguagem do navegador
+    function activeTab(index) {
+      tabConteudo.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabConteudo[index].classList.add("ativo");
+    }
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
 
-// Retorne a largura da janela 
+initTabNav();
+
+function initAcordo() {
+  const acordoLista = document.querySelectorAll(".js-acordo dt");
+  const activoClass = "ativo";
+  if (acordoLista.length) {
+    acordoLista[0].classList.add(activoClass);
+    acordoLista[0].nextElementSibling.classList.add(activoClass);
+
+    function activeAcordo() {
+      this.classList.toggle("activoClass");
+      this.nextElementSibling.classList.toggle(activoClass);
+    }
+    acordoLista.forEach((item) => {
+      item.addEventListener("click", activeAcordo);
+    });
+  }
+}
+initAcordo();
+
+function initiScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollTosection(event) {
+    event.preventDefault();
+    const href = event.correntTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollTosection);
+    });
+  }
+}
+initiScrollSuave();
