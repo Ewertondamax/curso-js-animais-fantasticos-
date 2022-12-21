874c0c56356719bbea5a95c300bd53eb7feeea74
Ewerton Martins da Silva <ewertondamax@gmail.com>
2022-12-20 21:19:06 -0300

    REFATORANDO TODO CODIGO E ADICIONANDO MODAL

--- js/script.js
+++ js/script.js
@@ -1,82 +1,18 @@
-function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabConteudo = document.querySelectorAll(".js-tabconteudo section");
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import * as teste from "./modules/teste.js";
+import initModal from "./modules/modal.js";
 
-  if (tabMenu.length && tabConteudo.length) {
-    tabConteudo[0].classList.add("ativo");
+teste.teste1();
+teste.teste2();
+console.log(teste.senha);
 
-    function activeTab(index) {
-      tabConteudo.forEach((section) => {
-        section.classList.remove("ativo");
-      });
-      tabConteudo[index].classList.add("ativo");
-    }
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => {
-        activeTab(index);
-      });
-    });
-  }
-}
+console.log(this);
 
-initTabNav();
-
-function initAcordo() {
-  const acordoLista = document.querySelectorAll(".js-acordo dt");
-  const activoClass = "ativo";
-  if (acordoLista.length) {
-    acordoLista[0].classList.add(activoClass);
-    acordoLista[0].nextElementSibling.classList.add(activoClass);
-
-    function activeAcordo() {
-      this.classList.toggle(activoClass);
-      this.nextElementSibling.classList.toggle(activoClass);
-    }
-    acordoLista.forEach((item) => {
-      item.addEventListener("click", activeAcordo);
-    });
-  }
-}
-initAcordo();
-
-function initiScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-
-  function scrollTosection(event) {
-    event.preventDefault();
-    const href = event.correntTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    const topo = section.offsetTop;
-
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-
-    linksInternos.forEach((link) => {
-      link.addEventListener("click", scrollTosection);
-    });
-  }
-}
-initiScrollSuave();
-
-function initAnimacaoScroll() {
-  const section = document.querySelectorAll(".js-ecroll");
-  if (section.length) {
-    const windowMetade = window.innerHeight * 0.6;
-    function animaScroll() {
-      section.forEach(() => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade > 0;
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else {
-          section.classList.remove("ativo");
-        }
-      });
-    }
-    animaScroll();
-    window.addEventListener("scroll", animaScroll);
-  }
-}
+initScrollSuave();
 initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
